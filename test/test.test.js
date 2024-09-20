5e5306045a18a59df451fae9705e6b7b2a406bc8
Maverick Nguyen <149219810+NguyenMav@users.noreply.github.com>
2024-09-20 13:49:24 +1000

    Update test.test.js

--- test/test.test.js
+++ test/test.test.js
@@ -1,22 +1,52 @@
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import app from './index.js';
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sqlite3 = require('sqlite3').verbose();
+const app = require('./index.js'); // Assuming your app is in index.js
+const sqlite3Memory = require('sqlite3-memory');
 
 chai.use(chaiHttp);
 const expect = chai.expect;
 
-describe('Subscribe Endpoint', () => {
-  it('should successfully subscribe an email', (done) => {
+describe('Database Tests', () => {
+  let db;
+
+  beforeEach(() => {
+    db = new sqlite3.Database(':memory:', (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log('Connected to in-memory database');
+    });
+  });
+
+  afterEach(() => {
+    db.close((err) => {
+      if (err) {
+        throw err;
+      }
+      console.log('Closed in-memory database');
+    });
+  });
+
+  it('should insert a new subscriber', (done) => {
     const email = 'test@example.com';
 
-    chai.request(app)
-      .post('/subscribe')
-      .send({ email })
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(201);
-        expect(res.text).to.equal('Subscription successful');
+    db.run('INSERT INTO Subscribers (email) VALUES (?)', [email], (err) => {
+      if (err) {
+        throw err;
+      }
+
+      db.all('SELECT * FROM Subscribers', [], (err, rows) => {
+        if (err) {
+          throw err;
+        }
+
+        expect(rows.length).to.equal(1);
+        expect(rows[0].email).to.equal(email);
         done();
       });
+    });
   });
+
+  // Add more test cases for other database operations
 });
