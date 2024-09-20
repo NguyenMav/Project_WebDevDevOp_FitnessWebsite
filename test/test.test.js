59e4f8d91d99da59ff85148597e355be2edaa76f
Maverick Nguyen <149219810+NguyenMav@users.noreply.github.com>
2024-09-20 02:38:13 +0000

    Update test

--- test/test.test.js
+++ test/test.test.js
@@ -1,17 +1,11 @@
-const chai = require('chai');
-const expect = chai.expect;
-
-// Sample function to test
 function add(a, b) {
     return a + b;
 }
 
 describe('Add function', function() {
     it('should return 3 when adding 1 and 2', function() {
-        expect(add(1, 2)).to.equal(3);
-    });
-
-    it('should return 5 when adding 2 and 3', function() {
-        expect(add(2, 3)).to.equal(5);
+        if (add(1, 2) !== 3) {
+            throw new Error('Test failed: expected 3');
+        }
     });
 });
